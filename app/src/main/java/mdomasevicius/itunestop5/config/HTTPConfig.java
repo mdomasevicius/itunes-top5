6af25a43f9b4f32d6ae03385ad0ad3c0b3558036
Mantas Domaševičius <mdo@platform-lunar.com>
2020-03-07 14:36:32 +0200

    Log okHttp request/response calls

--- app/src/main/java/mdomasevicius/itunestop5/config/HTTPConfig.java
+++ app/src/main/java/mdomasevicius/itunestop5/config/HTTPConfig.java
@@ -2,23 +2,29 @@ package mdomasevicius.itunestop5.config;
 
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static okhttp3.logging.HttpLoggingInterceptor.Level;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
 class HTTPConfig {
 
     @Bean
     OkHttpClient httpClient() {
+        Logger httpLog = getLogger("http.client");
         return new OkHttpClient.Builder()
             .connectTimeout(5, SECONDS)
             .retryOnConnectionFailure(true)
             .readTimeout(20, SECONDS)
             .writeTimeout(20, SECONDS)
             .connectionPool(new ConnectionPool(10, 10, MINUTES))
+            .addInterceptor(new HttpLoggingInterceptor(httpLog::info).setLevel(Level.BASIC))
             .build();
     }
 
